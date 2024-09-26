4eb1411aebead3f680b63cda81399ccf0e2e5ad6
samurollie <samuelbarbosa2001@gmail.com>
2024-09-26 12:17:05 -0300

    First Commit

---
+++ src/test/kotlin/com/clp/credit_card/CreditCardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clp.credit_card
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditCardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
