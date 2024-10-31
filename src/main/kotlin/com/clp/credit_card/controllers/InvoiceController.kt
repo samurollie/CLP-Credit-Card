54497e52199635aad8b91d9b9a7aba3bb7b1a646
samurollie <samuelbarbosa2001@gmail.com>
2024-10-31 18:53:31 -0300

    refactor: change from model to entity

--- src/main/kotlin/com/clp/credit_card/controllers/InvoiceController.kt
+++ src/main/kotlin/com/clp/credit_card/controllers/InvoiceController.kt
@@ -1,16 +1,27 @@
 package com.clp.credit_card.controllers
 
+import com.clp.credit_card.entities.InvoiceEntity
+import com.clp.credit_card.services.InvoiceService
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/invoices")
-class InvoiceController {
+class InvoiceController(private val invoiceService: InvoiceService) {
 
     @GetMapping
-    fun getInvoices(): ResponseEntity<String> {
+    fun getAllInvoices(): ResponseEntity<String> {
         return ResponseEntity.ok(":D")
+    }
+
+    @PostMapping
+    fun createInvoice(@RequestBody invoice: String): ResponseEntity<String> {
+//        val createdInvoice = invoiceService.createInvoice(invoice)
+        return ResponseEntity.ok("createdInvoice")
     }
 }
--- src/main/kotlin/com/clp/credit_card/models/PurchaseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.clp.credit_card.models
-
-import java.util.*
-
-data class PurchaseModel(
-    val data: Date,
-    val valor: Double,
-    val loja: String
-)
