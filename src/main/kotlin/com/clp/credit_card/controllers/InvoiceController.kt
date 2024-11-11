2b0e1f7f1c1ceeeed0ed67509e89615ad203fe15
Samuel Lucas <53665491+samurollie@users.noreply.github.com>
2024-11-11 07:39:30 -0300

    to do: retornar todas as faturas (com ou sem compras)

--- src/main/kotlin/com/clp/credit_card/controllers/InvoiceController.kt
+++ src/main/kotlin/com/clp/credit_card/controllers/InvoiceController.kt
@@ -1,15 +1,29 @@
 package com.clp.credit_card.controllers
 
+import com.clp.credit_card.controllers.PurchaseController.PurchaseResponse
 import com.clp.credit_card.services.InvoiceService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/invoices")
 class InvoiceController(private val invoiceService: InvoiceService) {
+    data class InvoiceResponse(
+        val value: Double,
+        val dueDate: LocalDate,
+        val closingDate: LocalDate,
+        val isPaid: Boolean,
+        val paymentDate: LocalDate,
+        val cardId: Int
+    )
+    sealed class InvoiceResponseWrapper {
+        data class Single(val invoice: InvoiceResponse) : InvoiceResponseWrapper()
+        data class Multiple(val invoice: InvoiceResponse) : InvoiceResponseWrapper()
+    }
 
     @GetMapping
-    fun getAllInvoices(): ResponseEntity<String> {
+    fun getAllInvoices(@PathVariable cardId: Int, @PathVariable withPurchases: Boolean): ResponseEntity<String> {
         return ResponseEntity.ok(":D")
     }
 
